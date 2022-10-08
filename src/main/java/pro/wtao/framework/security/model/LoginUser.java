7d47af52682a3d9b6681fd19e8cc5749e4982489
汪涛 <wangtao@360humi.com>
2022-10-08 15:29:07 +0800

    ✨: 重写UserDetails model

---
+++ src/main/java/pro/wtao/framework/security/model/LoginUser.java
@@ -0,0 +1,83 @@
+package pro.wtao.framework.security.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.UUID;
+
+/**
+ * <pre>
+ * <b></b>
+ * <b>Description:</b>
+ * <b>Copyright:</b> Copyright 2022 Wangtao. All rights reserved.
+ * <b>Changelog:</b>
+ *   Ver   		Date                    Author               	 Detail
+ *   ----------------------------------------------------------------------
+ *   1.0   2022/10/8 11:29    Wangtao     new file.
+ * </pre>
+ *
+ * @author Wangtao
+ * @since 2022/10/8
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginUser implements UserDetails {
+    private String userid;
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String phone;
+    private String email;
+    private Boolean enabled;
+    private Collection<UriGrantedAuthority> authorities;
+
+    private String jti;
+
+    public String getJti() {
+        jti = StringUtils.isBlank(jti) ? UUID.randomUUID().toString() : jti;
+        return jti;
+    }
+
+    @Override
+    public Collection<UriGrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+
+}
---
+++ src/main/java/pro/wtao/framework/security/model/UriGrantedAuthority.java
@@ -0,0 +1,69 @@
+package pro.wtao.framework.security.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Objects;
+
+/**
+ * <pre>
+ * <b>接口权限</b>
+ * <b>Description:</b>
+ * <b>Copyright:</b> Copyright 2022 Wangtao. All rights reserved.
+ * <b>Changelog:</b>
+ *   Ver   		Date                    Author               	 Detail
+ *   ----------------------------------------------------------------------
+ *   1.0   2022/10/8 11:30    Wangtao     new file.
+ * </pre>
+ *
+ * @author Wangtao
+ * @since 2022/10/8
+ */
+@Data
+public final class UriGrantedAuthority implements GrantedAuthority {
+
+    private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
+    private final RequestMethod method;
+    private final String uri;
+    private final String systemCode;
+
+    public UriGrantedAuthority(RequestMethod method, String uri, String systemCode) {
+        this.method = method;
+        this.uri = uri;
+        this.systemCode = systemCode;
+    }
+
+    @Override
+    public String getAuthority() {
+        return systemCode.concat(" ").concat(method.name()).concat(",").concat(uri);
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof UriGrantedAuthority) {
+            return (this.method.equals(((UriGrantedAuthority) obj).method)
+                    && (this.uri.equals(((UriGrantedAuthority) obj).uri))
+                    &&(this.systemCode.equals(((UriGrantedAuthority) obj).systemCode)));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(method, uri, systemCode);
+    }
+
+    @Override
+    public String toString() {
+        return this.getAuthority();
+    }
+
+
+}
