43f784e2fb994bf9585c195fc6c62ea5a8c08004
汪涛 <wangtao@360humi.com>
2022-10-08 15:30:03 +0800

    ✨: IOC容器持有者

---
+++ src/main/java/pro/wtao/framework/security/context/ApplicationContextHolder.java
@@ -0,0 +1,74 @@
+package pro.wtao.framework.security.context;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * <pre>
+ * <b>IOC容器持有者</b>
+ * <b>Description:</b>
+ * <b>Copyright:</b> Copyright 2022 Wangtao. All rights reserved.
+ * <b>Changelog:</b>
+ *   Ver   		Date                    Author               	 Detail
+ *   ----------------------------------------------------------------------
+ *   1.0   2022/10/8 15:18    Wangtao     new file.
+ * </pre>
+ *
+ * @author Wangtao
+ * @since 2022/10/8
+ */
+public class ApplicationContextHolder implements ApplicationContextAware {
+
+    /**
+     * 上下文对象实例
+     */
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextHolder.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取applicationContext
+     *
+     * @return
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 通过name获取 Bean.
+     *
+     * @param name
+     * @return
+     */
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    /**
+     * 通过class获取Bean.
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     *
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+}
