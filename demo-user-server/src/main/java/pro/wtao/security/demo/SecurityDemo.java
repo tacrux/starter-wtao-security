c3f9e8a37b516814f8f9acac248ff9eb83090020
汪涛 <wangtao@360humi.com>
2022-10-26 15:46:51 +0800

    🔒️: demo包扫描路径

--- demo-user-server/src/main/java/pro/wtao/security/demo/SecurityDemo.java
+++ demo-user-server/src/main/java/pro/wtao/security/demo/SecurityDemo.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 // 不指定包路径导致默认的安全过滤器链被加载
-@ComponentScan({"pro.wtao.framework.security"})
+@ComponentScan({"pro.wtao.framework.security","pro.wtao.security.demo"})
 public class SecurityDemo {
 
     public static void main(String[] args) {
