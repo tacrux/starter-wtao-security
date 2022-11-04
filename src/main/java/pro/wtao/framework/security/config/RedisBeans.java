3dc71ca04a2d08592165c8a43cbfb1320aab16fc
汪涛 <wangtao@360humi.com>
2022-11-04 17:57:10 +0800

    ✨: RedisTemplate不指定泛型，注入时指定

--- src/main/java/pro/wtao/framework/security/config/RedisBeans.java
+++ src/main/java/pro/wtao/framework/security/config/RedisBeans.java
@@ -64,4 +64,27 @@ public class RedisBeans {
         return template;
     }
 
+    /**
+     *  不指定泛型，注入时指定
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+
+        // 设置value的序列化规则和 key的序列化规则
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+
+    }
+
 }
